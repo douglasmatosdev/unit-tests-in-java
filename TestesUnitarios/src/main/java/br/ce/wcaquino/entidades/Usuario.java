d449230b0b888f60cadae89626b99dbe5d3d51d3
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-05 11:35:40 -0300

    main assertions and refactoring some tests

--- TestesUnitarios/src/main/java/br/ce/wcaquino/entidades/Usuario.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/entidades/Usuario.java
@@ -1,5 +1,7 @@
 package br.ce.wcaquino.entidades;
 
+import java.util.Objects;
+
 public class Usuario {
 
 	private String nome;
@@ -16,5 +18,18 @@ public class Usuario {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Usuario)) return false;
+		Usuario usuario = (Usuario) o;
+		return Objects.equals(nome, usuario.nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
 	}
 }
--- TestesUnitarios/src/test/java/br/ce/wcaquino/services/AssertTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/services/AssertTest.java
@@ -1,11 +1,57 @@
 package br.ce.wcaquino.services;
 
+import br.ce.wcaquino.entidades.Usuario;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AssertTest {
 
     @Test
-    public void test(){
+    public void test() {
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
 
+        Assert.assertEquals(1, 1);
+
+        // Para Double/float deve-se adicionar um terceiro parâmetro que é a margem de erro
+        Assert.assertEquals(0.51, 0.51, 0.01);
+        Assert.assertEquals(0.51234, 0.512, 0.001);
+        Assert.assertEquals(Math.PI, 3.14, 0.01);
+
+        // comparação de primitivos e classes
+        int i = 5;
+        Integer i2 = 5;
+        // Vai dar erro
+        // Assert.assertEquals(i, i2);
+
+        // Modo certo
+        Assert.assertEquals(Integer.valueOf(i), i2);
+        // outra forma
+        Assert.assertEquals(i, i2.intValue());
+
+        // String
+        Assert.assertEquals("bola", "bola");
+        // Mensagem descritiva
+        Assert.assertEquals("Apenas uma mensagem descritiva",  "bola", "bola");
+        Assert.assertNotEquals("bola", "casa");
+        Assert.assertTrue("bola".equalsIgnoreCase("BOLA"));
+        Assert.assertTrue("bola".startsWith("bo"));
+
+        // Objetos com instâncias diferentes mas é o mesmo uruário
+        Usuario u1 = new Usuario("Usuário 1");
+        Usuario u2 = new Usuario("Usuário 1");
+        Usuario u3 = u2;
+        Assert.assertEquals(u1, u2);
+
+        // Objetos com instâncias iguais mas não é o mesmo uruário
+        Assert.assertSame(u2, u3);
+        Assert.assertSame(u2, u2);
+        Assert.assertNotSame(u1, u2);
+
+        //Verificando se o Objeto é null
+        u3 = null;
+        Assert.assertTrue(u3 == null);
+        Assert.assertNull(u3);
+        Assert.assertNotNull(u1);
     }
 }
