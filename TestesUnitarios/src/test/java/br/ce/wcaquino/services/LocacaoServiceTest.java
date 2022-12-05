0e797a796665bb88d94b1bad38fc0e4df265f14a
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-05 14:46:44 -0300

    Assert.assertThat()

--- TestesUnitarios/src/test/java/br/ce/wcaquino/services/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/services/LocacaoServiceTest.java
@@ -5,6 +5,7 @@ import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,8 +23,15 @@ public class LocacaoServiceTest {
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         // verificação
+        Assert.assertThat(locacao.getValor(), CoreMatchers.is(5.0));
+        Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(6.0)));
+        Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(5.0)));
         Assert.assertEquals(5.0, locacao.getValor(), 0.01);
+
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
     }
 }
